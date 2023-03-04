00ae32a9b041a3b9d288ff812add9b3b35463a23
Yunus Olçar <yunusolcar@hotmail.com>
2023-03-04 20:23:36 +0300

    examples

---
+++ src/Daily examples/ex1.js
@@ -0,0 +1,14 @@
+
+var year = prompt("Lütfen doğum yılınızı giriniz");
+
+var dayYear = new Date();
+
+if (year <= 2005) {
+    document.write("Yaşınız ehliyet almak için uygundur.");
+} else {
+    (year > 2006)
+    document.write("Yaşınız ehliyet almak için <strong>uygun</strong> değildir! Kalan yıl: <b>" + (18 - (dayYear.getFullYear() - year)));
+}
+
+
+
